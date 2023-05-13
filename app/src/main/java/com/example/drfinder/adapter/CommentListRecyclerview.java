5eb3720b02a2f03c70d96c324decf0048616534e
ariya <ariyamanouchehri079@gmail.com>
2023-05-13 12:47:57 +0330

    set fade in and fade out for comment design

--- app/src/main/java/com/example/drfinder/adapter/CommentListRecyclerview.java
+++ app/src/main/java/com/example/drfinder/adapter/CommentListRecyclerview.java
@@ -3,6 +3,8 @@ package com.example.drfinder.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
@@ -18,10 +20,16 @@ import java.util.ArrayList;
 public class CommentListRecyclerview extends RecyclerView.Adapter<CommentListRecyclerview.Holder> {
     ArrayList<Appointment> appointmentArrayList = new ArrayList<>();
     CommentListRecyclerview.setOnClickListener listener;
+    Animation animationFadeIn;
+    Animation animationFadeOut;
+
 
     @NonNull
     @Override
     public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        animationFadeIn = AnimationUtils.loadAnimation(parent.getContext(), R.anim.fade_in);
+        animationFadeOut = AnimationUtils.loadAnimation(parent.getContext(), R.anim.fade_out);
+
         ItemCommentListBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_comment_list, parent, false);
         return new Holder(binding);
     }
@@ -49,11 +57,24 @@ public class CommentListRecyclerview extends RecyclerView.Adapter<CommentListRec
                 public void onClick(View v) {
                     if (binding.textInputLayout.getVisibility() == View.GONE) {
                         binding.arrow.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_up_24);
+
+                        binding.arrow.startAnimation(animationFadeIn);
+                        binding.textInputLayout.startAnimation(animationFadeIn);
+                        binding.submit.startAnimation(animationFadeIn);
+                        binding.rating.startAnimation(animationFadeIn);
+
                         binding.textInputLayout.setVisibility(View.VISIBLE);
                         binding.submit.setVisibility(View.VISIBLE);
                         binding.rating.setVisibility(View.VISIBLE);
+
                     } else {
                         binding.arrow.setBackgroundResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
+
+                        binding.arrow.startAnimation(animationFadeIn);
+                        binding.textInputLayout.startAnimation(animationFadeOut);
+                        binding.submit.startAnimation(animationFadeOut);
+                        binding.rating.startAnimation(animationFadeOut);
+
                         binding.textInputLayout.setVisibility(View.GONE);
                         binding.submit.setVisibility(View.GONE);
                         binding.rating.setVisibility(View.GONE);
